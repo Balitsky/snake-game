72c2f55f70616b8425929e0ba5761e12b6c9c85f
Balitsky <balitsky99@gmail.com>
2020-08-02 20:35:21 +0300

    game cycle clean up

--- core/GameCycle.py
+++ core/GameCycle.py
@@ -1,26 +1,17 @@
 import pygame
-from core.Window import Window
-from core.Controller import Controller
 
 class GameCycle:
 
 	def start():
 		pygame.init()		
-		window = Window(title = "Main Display")
-
-		controller = Controller(window)
 
 		clock = pygame.time.Clock()
 
 		while not GameCycle.gameEnd():
-
-			
-			controller.prepareNextStep()
-			window.update()
-
 			clock.tick(60)
 
 		pygame.quit()
+		quit()
 
 	def gameEnd():
 		for event in pygame.event.get():
