374f30fcbc1e8419cac0b4daac49aad59b8d6c57
Balitsky <balitsky99@gmail.com>
2020-08-04 00:23:21 +0300

    add widget debug

--- lib/Widget.py
+++ lib/Widget.py
@@ -6,15 +6,20 @@ from Box import Box
 class Widget:
     box: Box
     childs: []
+    parent: "Widget"
     background: ()
 
     def __init__(self):
         self.childs = []
+        self.parent = None
 
     def addChild(self, child):
         self.childs.append(child)
+        child.parent = self
 
     def paint(self, display):
+        if self.parent:
+            self.box.recalculate(self.parent.box)
         pygame.draw.rect(display, self.background, self.box)
         for child in self.childs:
             child.paint(display)
@@ -24,12 +29,15 @@ class Widget:
         self.box = Box(*args, **kwargs)
 
 
+
+
+# widget debug
 pygame.init()
 display = pygame.display.set_mode((500, 500))
 
 widget = Widget()
 widget.background = (0, 255, 0)
-widget.setLayout(0.25, 0.25, 0.5, 0.5, base = None, display = display)
+widget.setLayout(0.25, 0.25, 0.5, 0.5, display.get_rect())
 
 widget1 = Widget()
 widget1.background = (255, 0, 0)
@@ -40,9 +48,28 @@ widget2.background = (0, 0, 255)
 widget2.setLayout(0, 0, 0.2, 0.2, widget1.box)
 
 widget.addChild(widget1)
-widget.addChild(widget2)
+widget1.addChild(widget2)
 
 widget.paint(display)
 
 pygame.display.update()
 pygame.time.wait(2000)
+
+clock = pygame.time.Clock()
+
+def gameEnd():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return True
+        if pygame.key.get_pressed()[pygame.K_ESCAPE]:
+            return True
+
+while not gameEnd():
+    clock.tick(60)
+    widget1.box.offsetX += -0.005
+    widget1.box.recalculate(widget.box)
+    widget.paint(display) 
+    pygame.display.update()
+pygame.quit()
+quit()
+
