ef62b5511d3f53e72e07cbeadbc28ab5984d2500
Balitsky <balitsky99@gmail.com>
2020-08-12 20:49:43 +0300

    improve init logic

--- lib/Widget.py
+++ lib/Widget.py
@@ -13,6 +13,8 @@ class Widget(Sprite):
     def __init__(self):
         Sprite.__init__(self)
         self.box = Box()
+        self.rect = self.box
+        self.image = Surface((1, 1))
         self.childs = []
 
     def addChild(self, child):
@@ -29,14 +31,14 @@ class Widget(Sprite):
     def setLayout(self, *args, **kwargs):
         self.box.setLayout(*args, **kwargs)
 
-    def setBackground(self, color: str = None, image: str = None):
+    def setBackground(self, color: str = None, image: str = None, optimize = False):
         if image:
-            self.image = pygame.image.load(image).convert()
+            self.image = pygame.image.load(image)
+            if optimize:
+                self.image = self.image.convert()
         elif color:
-            self.image = Surface((1, 1))
             self.image.fill(Color(color))
         
     def update(self):
         self.box.recalculate()
-        self.rect = self.box
         self.image = pygame.transform.scale(self.image, (self.box.w, self.box.h))
