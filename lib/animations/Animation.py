48ad925cea5170f7e318b008bdedc114437f1bfa
Balitsky <balitsky99@gmail.com>
2020-08-14 17:18:20 +0300

    add abstract Animation class

---
+++ lib/animations/Animation.py
@@ -0,0 +1,17 @@
+
+# base abstract Animation class
+
+class Animation():
+
+	From
+	To
+	Duration
+
+	def From(self, From) -> "Animation":
+		self.From = From
+
+	def To(self, To) -> "Animation":
+		self.To = To
+
+	def Duration(self, Duration) -> "Animation":
+		self.Duration = Duration
